1fd707a7fc102b5fc3d78d6da99c29887b97026e
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-01-11 15:00:30 +0200

    building components and UI

---
+++ src/components/Calculator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../App.css';
+
+class Calculator extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <div className="child display-result dark-grey">0</div>
+        <button type="button" className="child light-grey">AC</button>
+        <button type="button" className="child light-grey">+/-</button>
+        <button type="button" className="child light-grey">%</button>
+        <button type="button" className="child orange">&#x00F7;</button>
+        <button type="button" className="child light-grey">7</button>
+        <button type="button" className="child light-grey">8</button>
+        <button type="button" className="child light-grey">9</button>
+        <button type="button" className="child orange">&times;</button>
+        <button type="button" className="child light-grey">4</button>
+        <button type="button" className="child light-grey">5</button>
+        <button type="button" className="child light-grey">6</button>
+        <button type="button" className="child orange">-</button>
+        <button type="button" className="child light-grey">1</button>
+        <button type="button" className="child light-grey">2</button>
+        <button type="button" className="child light-grey">3</button>
+        <button type="button" className="child orange">+</button>
+        <button type="button" className="child zero light-grey">0</button>
+        <button type="button" className="child light-grey">.</button>
+        <button type="button" className="child orange">=</button>
+      </div>
+    );
+  }
+}
+export default Calculator;
